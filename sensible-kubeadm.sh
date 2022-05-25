e85a1d0dfbf39d5f62164c2823ddcc5aa80427e3
Haf <haf@protonmail.ch>
2022-05-25 13:39:45 +0200

    added an init configuration

--- sensible-kubeadm.sh
+++ sensible-kubeadm.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env sh
 
+CONTROL_PLANE_ENDPOINT="$1"
+APISERVER_ADVERTISE_ADDRESS="$2"
 POD_CIDR=${3:-"10.0.0.0/16"}
 
 CONFIG_ROOT_DIR=/etc/kubernetes/kubeadm-config
 mkdir -p "$CONFIG_ROOT_DIR"
 ENCRYPTION_CONFIGURATION_FILE="$CONFIG_ROOT_DIR/encryptionconfiguration.yaml"
-CLUSTER_CONFIGURATION_FILE="$CONFIG_ROOT_DIR/clusterconfiguration.yaml"
+KUBEADM_CONFIG_FILE="$CONFIG_ROOT_DIR/kubeadmconfig.yaml"
 AUDIT_POLICY_FILE="$CONFIG_ROOT_DIR/policy.yaml"
 
 # TODO: Use files and envsubst instead of inlining the yamls here?
@@ -29,10 +31,11 @@ echo "Encryption configuration: $ENCRYPTION_CONFIGURATION_FILE"
 cp ./policy.yaml "$AUDIT_POLICY_FILE"
 echo "Audit policy configyration: $AUDIT_POLICY_FILE"
 
-cat <<EOF > "$CLUSTER_CONFIGURATION_FILE"
+cat <<EOF > "$KUBEADM_CONFIG_FILE"
 apiVersion: kubeadm.k8s.io/v1beta3
 kind: ClusterConfiguration
 kubernetesVersion: v1.24.0
+controlPlaneEndpoint="$CONTROL_PLANE_ENDPOINT"
 networking:
   podSubnet: "$POD_CIDR"
 featureGates:
@@ -46,6 +49,12 @@ apiServer:
 apiVersion: kubelet.config.k8s.io/v1beta1
 kind: KubeletConfiguration
 seccompDefault: true
+---
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: InitConfiguration
+localAPIEndpoint:
+  advertiseAddress: "$APISERVER_ADVERTISE_ADDRESS"
+  bindPort: 6443
 EOF
 
 echo "Cluster configuration: $CLUSTER_CONFIGURATION_FILE"
