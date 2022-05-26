7e4b4b6fd801f37bd7a325c21ec1511bb21ff32f
Haf <haf@protonmail.ch>
2022-05-26 21:41:54 +0200

    setting up hetzner csi driver too

--- setup-hetzner-ccm.sh
+++ setup-hetzner-ccm.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 set -eu
 
+# When in doubt, check this guide https://faun.pub/install-a-kubernetes-cluster-on-hetzner-cloud-200d4fb6a423
+
 echo "Setting up Hetzner's Cloud Controller Manager"
 
 read -rp "Enter your hcloud token: " HCLOUD_API_TOKEN
@@ -10,5 +12,8 @@ kubectl -n kube-system create secret generic hcloud --from-literal=token="$HCLOU
 
 kubectl apply -f https://github.com/hetznercloud/hcloud-cloud-controller-manager/releases/latest/download/ccm-networks.yaml
 
-# TODO: Set up Hetzner's CSI driver!
-# Follow this guide: https://faun.pub/install-a-kubernetes-cluster-on-hetzner-cloud-200d4fb6a423
+echo "Setting up Hetzner's CSI driver"
+
+kubectl -n kube-system create secret generic hcloud-csi --from-literal=token="$HCLOUD_API_TOKEN"
+
+kubectl apply -f https://raw.githubusercontent.com/hetznercloud/csi-driver/v1.6.0/deploy/kubernetes/hcloud-csi.yml
