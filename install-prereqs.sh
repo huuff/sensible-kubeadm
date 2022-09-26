3898a1b8fd7669f3b4a8702e64956a2ea8b1b918
Haf <haf@protonmail.ch>
2022-09-26 16:20:17 +0200

    better.

--- install-prereqs.sh
+++ install-prereqs.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 # TODO: Overridable by env vars
 containerd_version="1.6.5"
@@ -24,20 +25,22 @@ sudo sysctl --system
 
 echo "INSTALLING CONTAINERD..."
 wget "https://github.com/containerd/containerd/releases/download/v$containerd_version/containerd-$containerd_version-linux-amd64.tar.gz"
-tar -Cxzvf /usr/local "containerd-$containerd_version-linux-amd64.tar.gz" 
+tar Cxzvf /usr/local "containerd-$containerd_version-linux-amd64.tar.gz" 
+mkdir -p /usr/local/lib/systemd/system/
 curl "https://raw.githubusercontent.com/containerd/containerd/main/containerd.service" > /usr/local/lib/systemd/system/containerd.service
 systemctl daemon-reload
 systemctl enable --now containerd
 
 echo "INSTALLING RUNC..."
-curl "https://github.com/opencontainers/runc/releases/download/v$runc_version/runc.amd64"
+wget "https://github.com/opencontainers/runc/releases/download/v$runc_version/runc.amd64"
 install -m 755 runc.amd64 /usr/local/sbin/runc
 
 echo "INSTALLING CNI PLUGINS"
-curl "https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v$cni_plugins_version.tgz"
-tar -Cxzvf /opt/cni/bin "cni-plugins-linux-amd64-v$cni_plugins_version.tgz"
+wget "https://github.com/containernetworking/plugins/releases/download/v1.1.1/cni-plugins-linux-amd64-v$cni_plugins_version.tgz"
+tar Cxzvf /opt/cni/bin "cni-plugins-linux-amd64-v$cni_plugins_version.tgz"
 
 echo "SETTING SYSTEMD AS THE CGROUP DRIVER FOR CONTAINERD..."
+mkdir -p /etc/containerd
 cat <<EOF | tee /etc/containerd/config.toml
 [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
   [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
